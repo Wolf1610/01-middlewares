c758929585e73b59b7e89804700685c1b10b345d
Wolf1610 <dutta2772004@gmail.com>
2024-08-31 11:05:45 +0530

    01-middleware

---
+++ 03-errorcount.js
@@ -0,0 +1,39 @@
+
+const express = require('express');
+
+const app = express();
+let errorCount = 0;
+
+
+
+
+app.get("/user", function(req, res){
+  let a;
+  let b = a.length;
+  res.status(200).json({name: "Rihana"})
+})
+
+app.post('/user', function(req, res) {
+  res.status(200).json({ msg: 'created dummy user' });
+});
+
+app.get('/errorCount', function(req, res) {
+  res.status(200).json({ errorCount });
+});
+
+// -------------------
+// You have been given an express server which has a few endpoints.
+// Your task is to
+// 1. Ensure that if there is ever an exception, the end user sees a status code of 404
+// 2. Maintain the errorCount variable whose value should go up every time there is an exception in any endpoint
+
+
+app.use(function (err, req, res, next){
+  res.status(404).send({})
+  errorCount++;
+})
+
+// -------------------
+
+// app.listen(3000);
+module.exports = app;
